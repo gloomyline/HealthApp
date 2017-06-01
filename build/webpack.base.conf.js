7a69a8b0a14780fc461de378aa6c5ba4fadf031e
gloomyline <1211071880@qq.com>
2017-06-01 10:30:13 +0800

    extend the webpackConfig to apply vux-loader

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,12 +2,13 @@ var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
+var vuxLoader = require('vux-loader')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-module.exports = {
+const originConfig = {
   entry: {
     app: './src/main.js'
   },
@@ -65,3 +66,12 @@ module.exports = {
     ]
   }
 }
+
+module.exports = vuxLoader.merge(originConfig, {
+  options: {},
+  plugins: [
+    {
+      name: 'vux-ui'
+    }
+  ]
+})
