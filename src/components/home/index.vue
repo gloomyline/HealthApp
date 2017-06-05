ab8535c14167b70c081fdb3a8faf07fa62275277
gloomyline <1211071880@qq.com>
2017-06-05 18:41:42 +0800

    add better-scroll in home-page component

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -4,15 +4,16 @@
       <ul>
         <li v-for="(tech, index) in techList" class="tech-item">
           <div class="avatar">
-            <img width="73" height="73" :src="tech.AVATAR">
+            <img width="73" height="73" :src="tech.AVATAR" class="avatar-img">
+            <span v-show="tech.LEVEL === 1" class="level"></span>
           </div>
           <div class="content">
             <div class="title-wrapper">
               <span class="name">{{tech.NAME}}</span>
-              <span class="tody-about">{{tech.TODAY_ABOUT}}</span>
+              <span class="today-about">{{tech.TODAY_ABOUT | todayAbout}}</span>
             </div>
             <div class="info-wrapper">
-              <span class="age">{{tech.AGE}}</span>
+              <span class="age">{{tech.AGE}}岁</span>
               <dot></dot>
               <span class="registar">{{tech.REGISTAR}}</span>
               <dot v-show="tech.CERTIFICATE_IDS"></dot>
@@ -29,6 +30,7 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
   import dot from '@/components/uiComponents/dot'
 
   export default {
@@ -40,6 +42,37 @@
         }
       }
     },
+    data () {
+      return {
+        techs: []
+      }
+    },
+    created () {
+      this.$nextTick(() => {
+        this._initScroll()
+      })
+    },
+    watch: {
+      'techList' () {
+        this.$nextTick(() => {
+          this._initScroll()
+        })
+      }
+    },
+    methods: {
+      _initScroll () {
+        if (!this.scroll) {
+          this.scroll = new BScroll(this.$el.querySelector('.tech-list-hook'), {click: true})
+        } else {
+          this.scroll.refresh()
+        }
+      }
+    },
+    filters: {
+      todayAbout (num) {
+        return num === 0 ? '可约' : '不可约'
+      }
+    },
     components: {
       dot
     }
@@ -47,18 +80,62 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
   .home-page
-    position absolute
-    left 0
-    top 188px
-    width 100%
-    padding-bottom 49px
-    overflow hidden
     .tech-list-wrapper
+      position absolute
+      left 0
+      top 188px
+      bottom 49px
+      width 100%
+      overflow hidden
       .tech-item
         display flex
-      .avatar
-        flex 0 0 103px
-      .content
-        flex 1
+        padding 11px 15px
+        .avatar
+          flex 0 0 103px
+          position relative
+          .avatar-img
+            width 73px
+            height 73px
+          .level
+            display inline-block
+            position absolute
+            left 12px
+            bottom 15px
+            width 51px
+            height 14px
+            bg-img('level')
+            background-size contain
+        .content
+          flex 1
+          .title-wrapper
+            margin-bottom 16px
+            .name
+              margin-right 10px
+              font-size 15px
+              color #29292E
+            .today-about
+              display inline-block
+              width 50px
+              height 15px
+              line-height 15px
+              text-align center
+              padding 2px
+              border-radius 10px
+              font-size 10px
+              color #fff
+              background rgb(88, 79, 96)
+          .info-wrapper
+            margin-bottom 16px
+            font-size 12px
+            color #555
+          .desc-wrapper
+            margin-bottom 16px
+            overflow hidden
+            white-space nowrap
+            text-overflow ellipsis
+            font-size 11px
+            color #919191
 </style>
