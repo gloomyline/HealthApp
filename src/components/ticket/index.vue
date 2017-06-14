f644a5c632d0694926845062c55ee10ab13e5d41
gloomyline <1211071880@qq.com>
2017-06-14 16:29:40 +0800

    在页面路由中添加我的优惠券路由

---
+++ src/components/ticket/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="ticket-page">
+    <p>This is ticket page!</p>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default{}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .ticket-page
+    position fixed
+    left 0
+    top 0
+    width 100%
+    height 100%
+    z-index 100
+    background #fff
+</style>
