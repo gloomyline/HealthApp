310ebfff9c833be5749d45aa53fe7bb938eab748
gloomyline <1211071880@qq.com>
2017-06-22 17:14:49 +0800

    完成订单页套餐组件数据应用

--- src/components/order/item.vue
+++ src/components/order/item.vue
@@ -2,20 +2,20 @@
   <div class="order-item">
     <div class="content">
       <div class="content-left">
-        <img :src="item.img" width="98" height="76" class="item-img"
+        <img :src="itemImg" width="98" height="76" class="item-img"
              onerror="this.src='http://192.168.1.128:9999/static/imgs/default-item.png'">
       </div>
       <div class="content-right">
         <h2 class="item-name">
-          <span class="text">{{item.name}}</span>
+          <span class="text">{{itemName}}</span>
         </h2>
         <div class="star-wrapper">
-          <star class="star" :size="24" :score="item.score" has-score></star>
-          <span class="price">&yen;{{item.price}}/人</span>
+          <star class="star" :size="24" :score="Number(item.Star)" has-score></star>
+          <span class="price">&yen;{{item.Price}}/人</span>
         </div>
         <div class="extra">
-          <span class="desc">{{item.desc}}</span>
-          <span class="choose-count">已有{{item.chooseCount}}人选择</span>
+          <span class="desc">{{itemDesc}}</span>
+          <span class="choose-count">已有{{item.Num}}人选择</span>
         </div>
       </div>
     </div>
@@ -23,6 +23,7 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import { find } from '@/common/js/util'
   import star from '@/components/uiComponents/star'
   export default{
     props: {
@@ -37,6 +38,36 @@
             chooseCount: 200
           }
         }
+      }
+    },
+    data () {
+      return {
+        itemInfo: {}
+      }
+    },
+    computed: {
+      itemImg () {
+        if (this._findItemInfo() && this._findItemInfo().ItemImg) {
+          return this._findItemInfo().ItemImg.replace(/localhost:8080/, '192.168.1.126')
+        } else {
+          return ''
+        }
+      },
+      itemName () {
+        if (!this._findItemInfo()) return
+        return this._findItemInfo().ItemName
+      },
+      itemDesc () {
+        if (!this._findItemInfo()) return
+        return this._findItemInfo().ItemAbout
+      }
+    },
+    methods: {
+      _findItemInfo () {
+        if (!this.item.ItemId) return
+        let itemsConfig = this.getStaticConfig().itemList
+        let itemInfo = find(itemsConfig, _item => _item.ItemId === this.item.ItemId)
+        return itemInfo
       }
     },
     components: {
@@ -75,15 +106,10 @@
             vertical-align top
             margin-right 10px
             padding-top 4px
-          .score
-            display inline-block
-            vertical-align top
-            margin-right 14px
-            font-size 13px
-            color #fcd344
           .price
             display inline-block
             vertical-align top
+            margin-top 4px
             font-size 13px
             color #9c9c9c
         .extra
@@ -95,7 +121,7 @@
           .choose-count
             position absolute
             right 8px
-            bottom 12px
+            bottom 18px
             font-size 12px
             color #9c9c9c
 </style>
