e8f0fa38507254616d40cf8f3f3d06f85049dde4
jennifer-21 <1079286714@qq.com>
2017-06-28 16:13:51 +0800

    me interfacial effect

--- src/router/technicianClient/me.js
+++ src/router/technicianClient/me.js
@@ -5,9 +5,16 @@
 'use strict'
 
 import technicianMe from '@/views/technician/me'
-
+import information from '@/views/technician/me/information'
 export default {
   path: '/technician/me',
   name: 'technicianMe',
-  component: technicianMe
+  component: technicianMe,
+  children: [
+    {
+      path: '/information',
+      name: 'information',
+      component: information
+    }
+  ]
 }
